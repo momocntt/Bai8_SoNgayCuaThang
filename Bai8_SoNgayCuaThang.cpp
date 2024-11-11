41f3e70efb0e7d4d6eab494dd889b2e2bb7e5d8d
momocntt <nguyenthihoaimo1207@gmai.com>
2024-11-11 16:16:35 +0700

    Add project files.

---
+++ Bai8_SoNgayCuaThang.cpp
@@ -0,0 +1,38 @@
+// Bai8_SoNgayCuaThang.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int thu;
+	cout << "nhap thu"; cin >> thu;
+	switch (thu)
+	{
+	case 2:
+		cout << "thu hai\n";
+		break;
+	case 3:
+		cout << "thu ba\n";
+		break;
+	case 4:
+		cout << "thu tu\n";
+		break;
+	case 5:
+		cout << "thu nam\n";
+		break;
+	case 6:
+		cout << "thu sau\n";
+		break;
+	case 7:
+		cout << "thu bay\n";
+		break;
+	case 8:
+		cout << "chu nhat\n";
+		break;
+
+	}
+	return 0;
+}
